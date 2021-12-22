6b72e7a65167a93d674ba5f7352b6d4ba7ad8851
vihangkale <vihangk64@gmail.com>
2021-12-23 01:23:49 +0530

    added app drawer

--- src/App.js
+++ src/App.js
@@ -36,6 +36,10 @@ import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import Modal from '@mui/material/Modal';
 import RestartAltRoundedIcon from '@mui/icons-material/RestartAltRounded';
 import { withStyles, makeStyles } from "@material-ui/core/styles";
+import AndroidOutlinedIcon from '@mui/icons-material/AndroidOutlined';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+
 
 let bright= "";
 
@@ -44,6 +48,13 @@ function App() {
   const [darkTheme, setDarkTheme] = React.useState(false);
   let [brightness, setBrightness] = React.useState(100);
   const [modalOpen, setModalOpen] = React.useState(false);
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
 
   const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
@@ -118,8 +129,33 @@ const handleModalOpen = () => setModalOpen(true);
 const handleModalClose = () => setModalOpen(false);
 
 
+
+const toggleDrawer = (anchor, open) => (event) => {
+  if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    return;
+  }
+
+  setState({ ...state, [anchor]: open });
+};
+
+const appDrawer = (anchor) => (
+  <Box
+    sx={{width: 250 }}
+    role="presentation"
+    onClick={toggleDrawer("bottom", false)}
+    onKeyDown={toggleDrawer("bottom", false)}
+  >
+      <Stack direction="row" spacing={2}>
+      <Avatar alt="Remy Sharp" src="public/icons/gmail.png" />
+      <Avatar alt="Travis Howard" src="public/icons/gmail.png" />
+      <Avatar alt="Cindy Baker" src="public/icons/gmail.png" />
+    </Stack>
+
+  </Box>
+);
+
   return (
-    <div className="App" style={{backgroundColor: darkTheme ? "#000":null,filter: bright}}>
+    <div className="App" style={{backgroundColor: darkTheme ? "#000":"#d9e5e6",filter: bright}}>
       <AppBar position="fixed" >
        <Toolbar>
          <IconButton
@@ -131,9 +167,7 @@ const handleModalClose = () => setModalOpen(false);
          >
            <MenuIcon />
          </IconButton>
-         <Typography variant="h6" noWrap component="div">
-           Persistent drawer
-         </Typography>
+         <AndroidOutlinedIcon sx={{color:"#00e676",justifySelf: 'flex-end'}}/>
        </Toolbar>
       </AppBar>
      <Toolbar/>
@@ -304,15 +338,22 @@ const handleModalClose = () => setModalOpen(false);
         </Card>
         </Grid>
         <Grid item lg={4} md={4} sm={4} xs={4}>
-         <IconButton variant="contained" sx={{borderRadius:5,backgroundColor:"#424242",width:"100%",color:"#fff"}} >
+         <IconButton variant="contained" sx={{borderRadius:5,backgroundColor:"#2D3034",width:"100%",color: darkTheme ? "#fff":null}} >
             <ModeEditOutlineOutlinedIcon />
          </IconButton>
        </Grid>
        <Grid item lg={4} md={4} sm={4} xs={4}>
-        <IconButton onClick={handleModalOpen} variant="contained" sx={{borderRadius:5,backgroundColor:"#424242",width:"100%",color:"#fff"}} >
+        <IconButton onClick={handleModalOpen} variant="contained" sx={{borderRadius:5,backgroundColor:"#2D3034",width:"100%",color: darkTheme ? "#fff":null}} >
           <PowerSettingsNewIcon/>
         </IconButton>
-          <Modal
+      </Grid>
+        <Grid item lg={4} md={4} sm={4} xs={4}>
+         <IconButton variant="contained" sx={{borderRadius:5,backgroundColor:"#2D3034",width:"100%", color: darkTheme ? "#fff":null}} >
+            <SettingsOutlinedIcon/>
+         </IconButton>
+       </Grid>
+      </Grid>
+      <Modal
           open={modalOpen}
           onClose={handleModalClose}
           aria-labelledby="modal-modal-title"
@@ -320,12 +361,12 @@ const handleModalClose = () => setModalOpen(false);
         >
         <Box
             sx={{
-              backgroundColor:"#181b1f",
+              backgroundColor: darkTheme ? "#000":"#d9e5e6",
               position: 'absolute',
               top: '50%',
               left: '50%',
               p: 4,
-              width: 400,
+              width: "80vh",
               height:400,
               transform: 'translate(-50%, -50%)',
               borderRadius:10
@@ -333,30 +374,32 @@ const handleModalClose = () => setModalOpen(false);
             >
             <Grid container spacing={2} sx={{justifyContent:"center"}}>
             <Grid item lg={6} md={6} sm={6} xs={6} >
-              <IconButton variant="contained" sx={{borderRadius:"50%",backgroundColor:"#2D3034",height: "30vh",width:"100%",padding: "20px",color:"#fff"}} >
+              <IconButton variant="contained" sx={{borderRadius:"50%",backgroundColor:"#2D3034",height: "30vh",width:"100%",padding: "20px",color: darkTheme ? "#fff":null}} >
                 <PowerSettingsNewIcon/>
                 </IconButton>
             </Grid>
             <Grid item lg={6} md={6} sm={6} xs={6} >
-              <IconButton variant="contained" sx={{borderRadius:"50%",backgroundColor:"#2D3034",height: "30vh",width:"100%",padding: "20px",color:"#fff"}} >
+              <IconButton variant="contained" sx={{borderRadius:"50%",backgroundColor:"#2D3034",height: "30vh",width:"100%",padding: "20px",color: darkTheme ? "#fff":null}} >
                 <RestartAltRoundedIcon/>
                 </IconButton>
             </Grid>
             <Grid item lg={6} md={6} sm={6} xs={6}  >
-              <IconButton variant="contained" sx={{borderRadius:"50%",backgroundColor:"#2D3034",height: "30vh",width:"100%",padding: "20px",color:"#fff"}} >
+              <IconButton variant="contained" sx={{borderRadius:"50%",backgroundColor:"#2D3034",height: "30vh",width:"100%",padding: "20px",color: darkTheme ? "#fff":null}} >
                 <RestartAltRoundedIcon/>
                 </IconButton>
             </Grid>
           </Grid>
        </Box>
         </Modal>
-      </Grid>
-        <Grid item lg={4} md={4} sm={4} xs={4}>
-         <IconButton variant="contained" sx={{borderRadius:5,backgroundColor:"#424242",width:"100%", color:"#fff"}} >
-            <SettingsOutlinedIcon/>
-         </IconButton>
-       </Grid>
-      </Grid>
+
+          <Button onClick={toggleDrawer("bottom", true)}>bottom</Button>
+          <Drawer
+            anchor="bottom"
+            open={state["bottom"]}
+            onClose={toggleDrawer("bottom", false)}
+          >
+            {appDrawer("bottom")}
+          </Drawer>
       </Container>
     </Box>
     </div>
